73b639884b494ca0f55566ebe037f2a554631861
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-06-06 13:55:16 +0500

    bugs are fixed in faq section

--- js/main.js
+++ js/main.js
@@ -4,11 +4,20 @@ var elsButton = document.querySelectorAll('.qa__togler');
 elsButton.forEach(function (button) {
   button.addEventListener('click',function () {
 
-      elsQaItem.forEach(function (qa) {
-        qa.classList.remove('qa--active');
-      })
+      // elsQaItem.forEach(function (qa) {
+      //   qa.classList.remove('qa--active');
+      // })
 
-      button.closest('.qa').classList.add('qa--active');
+      button.closest('.qa').classList.toggle('qa--active');
   })
 })
 
+
+
+var elFeaturesItem = document.querySelectorAll('.features__item');
+
+elFeaturesItem.forEach(function (featureItem) {
+  featureItem.addEventListener('click',function() {
+    featureItem.classList.add('features__item--active');
+  })
+})
